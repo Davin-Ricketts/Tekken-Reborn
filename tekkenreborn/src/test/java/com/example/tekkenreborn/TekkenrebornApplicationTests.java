cf2e0ade512819e5a844db011638f28bfdfde63c
Davin-Ricketts <davinricketts32@gmail.com>
2023-01-26 22:10:42 -0500

    Lab 1

---
+++ tekkenreborn/src/test/java/com/example/tekkenreborn/TekkenrebornApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tekkenreborn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TekkenrebornApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
